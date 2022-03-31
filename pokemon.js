199f8d8a4b49aef5bdb44b04339c13db8648a91a
Yessica Chuctaya <yessica.chuctaya@ucsp.edu.pe>
2022-03-30 22:29:15 -0500

    ultimos cambios

--- pokemon.js
+++ pokemon.js
@@ -1,7 +1,7 @@
 
-import fetch from 'node-fetch';
+//import fetch from 'node-fetch';
 
-const pokeContent = document.getElementById('pokemonContent');
+//const pokeContent = document.getElementById('pokemonContent');
 /*pintar card pokemon*/
 const colors = {
     fire: '#FFA05D',
@@ -18,19 +18,67 @@ const colors = {
 	fighting: '#FF5D5D',
 	normal: '#FFFFFF'
 }
+const typeColors = {
+    electric: '#FFEA70',
+    normal: '#B09398',
+    fire: '#FF675C',
+    water: '#0596C7',
+    ice: '#AFEAFD',
+    rock: '#999799',
+    flying: '#7AE7C7',
+    grass: '#4A9681',
+    psychic: '#FFC6D9',
+    ghost: '#561D25',
+    bug: '#A2FAA3',
+    poison: '#795663',
+    ground: '#D2B074',
+    dragon: '#DA627D',
+    steel: '#1D8A99',
+    fighting: '#2F2F2F',
+    default: '#2A1A1F',
+};
 const URI = 'https://pokeapi.co/api/v2/pokemon/'
 
-const getPokemonName = async(name)=>{
-	const informacion = await fetch(URI+name.toLowerCase())
+const getPokemonName = async(e)=>{
+    e.preventDefault()
+
+    const inputPokemon = document.getElementById('inputPokemon').value;
+	const informacion = await fetch(URI+inputPokemon.toLowerCase())
+    
 	const info = await informacion.json()
-	console.log('name :',info.name)
-    console.log('id :',info.id)
-    info['types'].forEach(element => 
-        console.log('    type:',element['type']['name'])
-        );
+    document.getElementById("name_").innerHTML = info.name;
+    document.getElementById('id_').innerHTML = '#0'+info.id;
+    const sprite =  info.sprites.front_default;
+    document.querySelector('[data-poke-img]').setAttribute('src', sprite);
+
+    
+    
+    let typebd = document.getElementById('type')
+    let typeStr = ''
+    info['types'].forEach(element =>
+        typeStr += '<span>'+element['type']['name'] +'</span>'
+        /*const typeTextElement = document.createElement("span");
+        typeTextElement.style.color = typeColors[element.type.name];
+        typeTextElement.textContent = element.type.name;
+        typebd.appendChild(typeTextElement);*/
+    );
+    document.getElementById('type').innerHTML =  typeStr
+    
+    
+
+    const colorOne = typeColors[info['types'][0].type.name];
+    const colorTwo = info['types'][1] ? typeColors[info['types'][1].type.name] : typeColors.default;
+    document.querySelector('[data-poke-img]').style.background =  `radial-gradient(${colorTwo} 33%, ${colorOne} 33%)`;
+    document.querySelector('[data-poke-img]').style.backgroundSize = ' 5px 5px';
+    
+    /* <p>My mother has <span style="color:blue;font-weight:bold">blue</span> eyes and my father has <span style="color:darkolivegreen;font-weight:bold">dark green</span> eyes.</p>*/
+    let stateStr = '<p>'
     info['stats'].forEach(element => 
-            console.log(element['stat']['name'],':',element['base_stat'])
-            );
+        stateStr += element['stat']['name']  +'<span>'+element['base_stat'] +'</span>'
+        );
+    stateStr += '</p>'
+    console.log(stateStr)
+    document.getElementById('state').innerHTML = stateStr
 }
 
 const getPokemonId = async(id)=>{
@@ -44,7 +92,7 @@ const getPokemonId = async(id)=>{
     info['stats'].forEach(element => 
             console.log(element['stat']['name'],':',element['base_stat'])
             );
-    createPokemon(info, modal)
+    //createPokemon(info, modal)
 }
 
 
@@ -56,64 +104,4 @@ const drawPokemon = async () =>{
 }
 
 //getPokemonName('pikachu')
-drawPokemon()
-
-
-function  createPokemon(pokemon, modal){
-    const pokemonEl = document.createElement('div');
-    
-	pokemonEl.classList.add('pokemon');
-    
-	const poke_types = pokemon.types.map(type => type.type.name);
-	const type = main_types.find(type => poke_types.indexOf(type) > -1);
-	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-	const color = colors[type];
-	
-	pokemonEl.style.backgroundColor = color;
-
-	
-
-	if (modal !==true){
-        const pokeInnerHTML = `
-        <div class="img-container">
-            <img src="https://pokeres.bastionbot.org/images/pokemon/${
-							pokemon.id
-						}.png" alt="${name}" />
-        </div>
-        <div class="info">
-            <span class="number">#${pokemon.id
-							.toString()
-							.padStart(3, '0')}</span>
-            <h3 class="name">${name}</h3>
-            <small class="type">Tipo: <span>${type}</span></small>
-        </div>
-    `;
-        pokemonEl.innerHTML = pokeInnerHTML;
-        pokeContent.appendChild(pokemonEl);
-    }
-
-    else{
-        const pokeInnerHTML = `
-        <div class="modal" id="modalPokemon">
-        <div class="pokemon">
-        <div class="img-container">
-            <img src="https://pokeres.bastionbot.org/images/pokemon/${
-							pokemon.id
-						}.png" alt="${name}" />
-        </div>
-        <div class="info">
-            <span class="number">#${pokemon.id
-							.toString()
-							.padStart(3, '0')}</span>
-            <h3 class="name">${name}</h3>
-            <small class="type">Tipo: <span>${type}</span></small>
-        </div>
-        </div>
-    
-    </div>`;
-                
-
-    modalSearch.innerHTML = pokeInnerHTML;
-        
-    }
-}
+//drawPokemon()
